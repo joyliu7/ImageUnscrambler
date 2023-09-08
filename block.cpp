95e86abae4fbe2c2b8cf8c8a0ffd2e4c18f12de1
Joy Liu <141117721+joyliu7@users.noreply.github.com>
2023-09-07 21:06:45 -0700

    Add files via upload

---
+++ block.cpp
@@ -0,0 +1,21 @@
+#include "block.h"
+#include <cmath>
+#include <iostream>
+
+int Block::height() const {
+  /* your code here */
+  return 0;
+}
+
+int Block::width() const {
+  /* your code here */
+  return 0;
+}
+
+void Block::render(PNG &im, int x) const {
+  /* your code here */
+}
+
+void Block::build(PNG &im, int x, int width) {
+  /* your code here */
+}
